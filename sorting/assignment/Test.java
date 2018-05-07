66308113f245d16c547d8158b1b11e3cd3b09428
Jason_Wu <jason_wujiakun@yeah.net>
2018-05-07 22:45:24 +0800

    collinear v1

---
+++ sorting/assignment/Test.java
@@ -0,0 +1,7 @@
+package sorting.assignment;
+
+public class Test {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ sorting/elementarysorts/MergeHalfAux.java
@@ -0,0 +1,65 @@
+package sorting.elementarysorts;
+
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class MergeHalfAux {
+	// this version is thread dangerous!
+	private static Comparable[] aux;
+
+	public static int mergeAndCount(Comparable[] a, int lo, int mid, int hi) {
+		for (int k = lo; k <= mid; k++) {
+			aux[k] = a[k];
+		}
+		int count = 0;
+		int i = lo;
+		int j = mid + 1;
+		for (int k = lo; k <= hi; k++) {
+			if (i > mid) {
+				// first half subarray has already merged
+				// a[k] = aux[j];
+				j++;
+			} else if (j > hi) {
+				// second half subarray has already merged
+				a[k] = aux[i];
+				i++;
+			} else if (Example.less(aux[i], a[j])) {
+				// a[i] is smaller, so merge a[i] into a
+				a[k] = aux[i];
+				i++;
+			} else {
+				a[k] = a[j];
+				count += mid - i + 1;
+				j++;
+			}
+		}
+		return count;
+	}
+
+	public static int sortAndCount(Comparable[] a) {
+		aux = new Comparable[a.length];
+		return sortAndCount(a, 0, a.length - 1);
+	}
+
+	public static int sortAndCount(Comparable[] a, int lo, int hi) {
+		if (hi <= lo)
+			return 0;
+		int mid = lo + (hi - lo) / 2;
+		int lowCount = sortAndCount(a, lo, mid);
+		int hiCount = sortAndCount(a, mid + 1, hi);
+		return mergeAndCount(a, lo, mid, hi)+lowCount + hiCount;
+	}
+
+	public static void main(String[] args) {
+		Double[] a = new Double[Integer.parseInt(args[0])];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = (double) StdRandom.uniform(a.length);
+		}
+		StdOut.println(Arrays.toString(a));
+		StdOut.println(sortAndCount(a));
+		StdOut.println(Arrays.toString(a));
+
+	}
+}
